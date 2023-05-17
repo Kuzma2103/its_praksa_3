8a362c61afaec7d0250389a80f151f3a9b008e69
Ivan Kuzmanović <ivan.kuzmanovic@comtrade.com>
2023-05-17 13:43:22 +0200

    initial commit

---
+++ Pages/BasePage.cs
@@ -0,0 +1,100 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace AutomationFramework.Pages
+{
+    public class BasePage
+    {
+        // Driver
+        public IWebDriver? driver;
+        public WebDriverWait wait;
+
+
+        /// <summary>
+        /// Metoda koja ceka vidljivost elementa
+        /// </summary>
+        private void WaitElementVisibility(By elementBy)
+        {
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(elementBy));
+        }
+
+        /// <summary>
+        /// Metoda koja klikne na element
+        /// </summary>
+        public void ClickOnElement(By element)
+        {
+            WaitElementVisibility(element);
+            driver.FindElement(element).Click();
+        }
+
+        /// <summary>
+        /// Metoda koja upisuje vrednost u polje
+        /// </summary>
+        public void WriteTextToElement(By element, string text)
+        {
+            WaitElementVisibility(element);
+            driver.FindElement(element).SendKeys(text);
+        }
+
+        /// <summary>
+        /// Metoda koja cita text iz elementa
+        /// </summary>
+        public string ReadTextFromElement(By element)
+        {
+            WaitElementVisibility(element);
+            return driver.FindElement(element).Text;
+        }
+
+        /// <summary>
+        /// Metoda koja cita text iz alert box-a
+        /// </summary>
+        public string ReadTextFromAlertBox()
+        {
+            Thread.Sleep(1000);
+            return driver.SwitchTo().Alert().Text;
+        }
+
+        /// <summary>
+        /// Metoda koja potvrdjuje alert box
+        /// </summary>
+        public void AcceptAlertBox()
+        {
+            Thread.Sleep(1000);
+            driver.SwitchTo().Alert().Accept();
+        }
+
+        /// <summary>
+        /// Metoda koja vraca vrednosti celija iz prvog reda u tabeli
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetValuesFromFirstTableRow()
+        {
+            List<string> values = new List<string>();
+            ReadOnlyCollection<IWebElement> rows = driver.FindElements(By.XPath("//tr[@class='success']"));
+
+            try
+            {
+                IWebElement firstRow = rows[0];
+                ReadOnlyCollection<IWebElement> cells = driver.FindElements(By.XPath("//tr[@class='success']/td"));
+
+                foreach (IWebElement cell in cells)
+                {
+                    values.Add(cell.Text);
+                }
+            } catch (Exception)
+            {
+                throw;
+            }
+
+            return values;
+        }
+        
+    }
+    
+}
